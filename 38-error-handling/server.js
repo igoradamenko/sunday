8a7d24800a155f897b3c004232ccd32e10919c4e
i.adamenko <i.adamenko@fun-box.ru>
2019-02-20 13:54:27 +0300

    Add 38-error-handling.

---
+++ 38-error-handling/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('index.html', { encoding: 'utf-8' }, (err, info) => {
+      if (err) {
+        console.error(err);
+        res.statusCode = 500;
+        res.end('There is an error on the server side');
+        return;
+      }
+
+      res.end(info);
+    });
+  } else if (req.url === '/now') {
+    res.end(new Date().toString());
+  }
+}).listen(3000);
