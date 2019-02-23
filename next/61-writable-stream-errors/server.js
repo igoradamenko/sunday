0ddfb7b6b5a678c015bf49927dff9d48b3083b99
i.adamenko <i.adamenko@fun-box.ru>
2019-02-23 20:38:56 +0200

    Add 61-writable-stream-errors.

---
+++ next/61-writable-stream-errors/server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+const fs = require('fs');
+
+new http.Server((req, res) => {
+  if (req.url === '/big.html') {
+    const file = new fs.ReadStream(__dirname + '/../53-readable-stream-example-big/big.html');
+    sendFile(file, res);
+  }
+}).listen(3000);
+
+function sendFile(file, res) {
+  file.pipe(res);
+  file.on('error', err => {
+    res.statusCode = 500;
+    res.end('Server Error');
+    console.error(err);
+  })
+}
