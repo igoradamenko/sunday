6307da37838148903b64a9f005e5652b39a561c7
i.adamenko <i.adamenko@fun-box.ru>
2019-02-24 17:22:24 +0200

    Beautify code of get-post-server-task.

--- 2019-02-24/64-repeat/get-post-server-task/index.js
+++ 2019-02-24/64-repeat/get-post-server-task/index.js
@@ -1,53 +1,50 @@
-/**
+/*
  ЗАДАЧА
- Написать HTTP-сервер для загрузки и получения файлов
- - Все файлы находятся в директории files
+
+ Написать HTTP-сервер для загрузки и получения файлов.
+
+ - Все файлы находятся в директории files.
  - Структура файлов НЕ вложенная.
 
- - Виды запросов к серверу
+ - Виды запросов к серверу:
    GET /file.ext
-   - выдаёт файл file.ext из директории files,
-   - правильный mime-type по содержимому (модуль mime)
+   - выдаёт файл file.ext из директории files;
+   - правильный mime-type по содержимому (модуль mime).
 
    POST /file.ext
-   - пишет всё тело запроса в файл files/file.ext и выдаёт ОК
-   - если файл уже есть, то выдаёт ошибку 409
-   - при превышении файлом размера 1MB выдаёт ошибку 413
+   - пишет всё тело запроса в файл files/file.ext и выдаёт ОК;
+   - если файл уже есть, то выдаёт ошибку 409;
+   - при превышении файлом размера 1MB выдаёт ошибку 413;
+   - так как поддиректорий нет, то при наличии / или .. в пути сервер должен выдавать ошибку 400;
+   - вместо file может быть любое имя файла.
 
- Вместо file может быть любое имя файла.
- Так как поддиректорий нет, то при наличии / или .. в пути сервер должен выдавать ошибку 400.
-
-- Сервер должен корректно обрабатывать ошибки "файл не найден" и другие (ошибка чтения файла)
-- index.html или curl для тестирования
-
-
- */
+ - Сервер должен корректно обрабатывать ошибки "файл не найден" и другие (ошибка чтения файла).
+ - index.html или curl для тестирования.
+*/
 
 // Пример простого сервера в качестве основы
 
-'use strict';
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 
-let url = require('url');
-let fs = require('fs');
-
-require('http').createServer(function(req, res) {
-
-  let pathname = decodeURI(url.parse(req.url).pathname);
+http.createServer((req, res) => {
+  const pathname = decodeURI(url.parse(req.url).pathname);
 
   switch(req.method) {
-  case 'GET':
-    if (pathname === '/') {
-      // отдачу файлов следует переделать "правильно"
-      fs.readFile(__dirname + '/public/index.html', (err, content) => {
-        if (err) throw err;
-        res.setHeader('Content-Type', 'text/html;charset=utf-8');
-        res.end(content);
-      });
-      return;
-    }
-  default:
-    res.statusCode = 502;
-    res.end("Not implemented");
-  }
+    case 'GET':
+      if (pathname === '/') {
+        // отдачу файлов надо сделать «правильно»
+        fs.readFile(`${__dirname}/public/index.html`, (err, content) => {
+          if (err) throw err;
+          res.setHeader('Content-Type', 'text/html;charset=utf-8');
+          res.end(content);
+        });
+        return;
+      }
 
+    default:
+      res.statusCode = 502;
+      res.end('Not implemented');
+  }
 }).listen(3000);
