cbec9c31ba13420f5ffc0bf1a9322888dfd26b74
i.adamenko <i.adamenko@fun-box.ru>
2019-03-10 13:19:08 +0300

    Add 70-pipe.

--- next/70-pipe/index.js
+++ next/70-pipe/index.js
@@ -3,12 +3,4 @@ const fs = require('fs');
 const fileIn = fs.createReadStream(__filename);
 const fileOut = fs.createWriteStream(`${__filename}.out`);
 
-fileIn.on('data', data => {
-  fileOut.write(data);
-});
-
-fileIn.on('close', () => {
-  fileOut.close();
-});
-
-//fileIn.pipe(fileOut);
+fileIn.pipe(fileOut);
