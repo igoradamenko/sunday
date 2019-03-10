d5d34dc160d5a8c6d5cd3515c7b6e95fc5d6568d
i.adamenko <i.adamenko@fun-box.ru>
2019-03-10 13:19:02 +0300

    Add 68-9-byte-fixed.

--- next/68-9-byte-fixed/index.js
+++ next/68-9-byte-fixed/index.js
@@ -1,7 +1,10 @@
+// 1
+//
 const fs = require('fs');
 
 const fileStream = fs.createReadStream(__dirname + '/data.txt', {
   highWaterMark: 9,
+  encoding: 'utf-8',
 });
 
 let content = '';
@@ -12,3 +15,31 @@ fileStream.on('data', data => {
 fileStream.on('end', () => {
   console.log(content);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+// 2
+//
+// const fs = require('fs');
+//
+// const fileStream = fs.createReadStream(__dirname + '/data.txt', {
+//   highWaterMark: 9,
+// });
+//
+// let buffers = [];
+// fileStream.on('data', data => {
+//   buffers.push(data)
+// });
+//
+// fileStream.on('end', () => {
+//   console.log(Buffer.concat(buffers).toString());
+// });
