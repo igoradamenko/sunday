51e75687496661afeaba2cd5202d86615d3361d4
i.adamenko <i.adamenko@fun-box.ru>
2019-06-12 16:46:39 +0300

    Add 91-promise-http-get.

---
+++ 2019-06-12/91-promise-http-get/index.js
@@ -0,0 +1,24 @@
+const http = require('http');
+
+function loadUrl(url) {
+  return new Promise((resolve, reject) => {
+    http.get(url, res => {
+      if (res.statusCode !== 200) {
+        reject(new Error(`Bad response status ${res.statusCode}.`));
+        return;
+      }
+
+      let body = '';
+      res.setEncoding('utf8');
+      res.on('data', chunk => (body += chunk));
+      res.on('end', () => resolve(body));
+
+    }) // ENOTFOUND (no such host) or ECONNRESET (server destroys connection)
+      .on('error', reject);
+  });
+}
+
+loadUrl('http://ya.ru').then(
+  result => console.log('Result:', result),
+  error => console.error('Error:', error)
+);
